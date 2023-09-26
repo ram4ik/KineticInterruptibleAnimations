d64e06e19cdc8e5f10498ef16c98e494ce801e2f
Ramill <ram4ik@hotmail.com>
2023-09-26 21:33:59 +0300

    Initial Commit

---
+++ KineticInterruptibleAnimations/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KineticInterruptibleAnimations
+//
+//  Created by Ramill Ibragimov on 9/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KineticInterruptibleAnimations/KineticInterruptibleAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  KineticInterruptibleAnimationsApp.swift
+//  KineticInterruptibleAnimations
+//
+//  Created by Ramill Ibragimov on 9/26/23.
+//
+
+import SwiftUI
+
+@main
+struct KineticInterruptibleAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
